77b24be771aab0c8aa44737a5db4168bf067d7a1
EstebanRoman99 <estebanromanh1999@gmail.com>
2025-01-03 02:21:46 -0600

    Corrigiendo errores de render backend

--- backend/src/main/java/com/example/demo/config/CorsGlobalConfig.java
+++ backend/src/main/java/com/example/demo/config/CorsGlobalConfig.java
@@ -10,11 +10,13 @@ public class CorsGlobalConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            // Aquí lista cada dominio donde el frontend corra
-            .allowedOrigins("http://localhost:5173", "https://appnotes-icqw.vercel.app")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "https://appnotes-icqw.vercel.app" 
+            )
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(true); // <-- Esto hace que Spring devuelva "Access-Control-Allow-Credentials: true"
+            .allowCredentials(false); // 
     }
 }
 
